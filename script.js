0e10672b031305c123c1aad5a15b5baeb5d27971
Eduardo Bordin <dudubordin26@gmail.com>
2022-04-13 01:51:28 -0300

    Dragon-Ball Project Added

---
+++ script.js
@@ -0,0 +1,22 @@
+let imgTeste = document.getElementById('image');
+
+function goku(){
+
+    //alert('ok');
+    imgTeste.src = "Goku.png";
+}
+function vegeta(){
+
+    //alert('ok');
+    imgTeste.src = "Vegeta.png";
+}
+function majinboo(){
+
+    //alert('ok');
+    imgTeste.src = "MajinBoo.png";
+}
+
+function refreshBtn(){
+
+    document.location.reload();
+}
